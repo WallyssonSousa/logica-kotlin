41d80f001c760d548164ecf36a74d9463036c9a2
Wallysson <wallysson.dev@gmail.com>
2025-03-01 22:22:24 -0300

    Exe 18.kt

---
+++ condicoes/exe18.kt
@@ -0,0 +1,20 @@
+fun main() {
+    print("Digite sua altura (em metros): ")
+    val altura = readln()!!.toDouble()
+    print("Digite seu peso (em kg): ")
+    val peso = readln()!!.toDouble()
+    val imc = peso / (altura * altura)
+
+    
+    if (imc < 18.5) {
+        println("Abaixo do peso")
+    } else if (imc >= 18.5 && imc < 25) {
+        println("Peso ideal")
+    } else if (imc >= 25 && imc < 30) {
+        println("Sobrepeso")
+    } else if (imc >= 30 && imc < 40) {
+        println("Obesidade")
+    } else {
+        println("Obesidade mórbida")
+    }
+}
